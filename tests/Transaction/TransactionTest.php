f1b7789163f64095184b0f441fb3c692486e5c1f
xGrz <grzesiek.byskiniewicz@gmail.com>
2026-09-05 10:36:54 +0200

    fix: Fixed namespace

--- tests/Transaction/TransactionTest.php
+++ tests/Transaction/TransactionTest.php
@@ -2,9 +2,10 @@
 
 namespace Xgrz\PayNow\Tests\Transaction;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Xgrz\PayNow\Tests\PayNowTestCase;
 
 class TransactionTest extends PayNowTestCase
 {
-
+    use RefreshDatabase;
 }
