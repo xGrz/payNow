19fa89615ea277343e891cd9918710ee47e0787f
xGrz <grzesiek.byskiniewicz@gmail.com>
2026-09-24 16:15:38 +0200

    feat: log PayNow payment notifications in `NotificationWebhookController`
    - Added logging to track incoming PayNow notifications for better traceability.

--- src/Http/Controllers/NotificationWebhookController.php
+++ src/Http/Controllers/NotificationWebhookController.php
@@ -3,6 +3,7 @@
 namespace Xgrz\PayNow\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Xgrz\PayNow\Events\PayNowPaymentStatusChangedEvent;
 use Xgrz\PayNow\Facades\PayNow;
 use Xgrz\PayNow\Models\PayNowAttempt;
@@ -12,6 +13,8 @@ class NotificationWebhookController
 {
     public function __invoke(Request $request)
     {
+        Log::info('PayNow notification received', $request->json('paymentId'));
+
         $consumed = PayNow::handleNotification($request);
         if ($consumed) {
 
